2a0cba6dac13e7fc5b8e48b0bfe78d6a0ea9eed6
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-12-14 09:58:10 -0600

    Adding image and css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,20 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          },
+        ],
+      },
+    ],
+  },
 };
